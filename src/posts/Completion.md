d30d20ec6d6d660b991b19b799b382fa19234b6d
Mattia Gasparotto <gasmat04@gmail.com>
2025-08-15 16:31:17 +0200

    Add theme toggle button

---
+++ src/posts/Completion.md
@@ -0,0 +1,9 @@
+---
+title: Completion of the website
+description: On this day the site is completed.
+date: '2025-08-15'
+published: true
+---
+
+The site is now complete! Some feature that I might add are a theme toggle button. 
+Of course, I will add more posts and guides as time goes on.
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -17,7 +17,7 @@
 	<!-- Navigation bar -->
 	<div class="container mx-auto flex flex-row items-center justify-between rounded-4xl">
 		<!-- Main container -->
-		<div class="px-4 pt-2 text-left text-2xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-7xl transition duration-300 ease-in-out hover:scale-110 inline">
+		<div class="px-4 pt-2 text-left text-2xl sm:text-4xl lg:text-5xl xl:text-7xl transition duration-300 ease-in-out hover:scale-110 inline">
 			<!-- Logo -->
 			<a href="/" class="heading-font flex items-center"
 			>$ Portfolio</a>
